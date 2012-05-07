577d61069d44d19c33048e6e45488d8ca21bc37e
anoop <anoop@ozonetel.com>
2012-05-07 12:26:17 +0530

    session_start(); updated

--- kookoophp/snippets/User joining a Dynamic conference by entering pin.php
+++ kookoophp/snippets/User joining a Dynamic conference by entering pin.php
@@ -1,12 +1,13 @@
 <?php
 //Description:Sample Code implementing usage of conference dynamically
 require_once("response.php");//response.php is the kookoo xml preparation class file
+session_start();
 $r = new Response();
 $confno =array(1234,3424); // declaration of array
-if($_REQUEST['event']== "NewCall" || $_SESSION['next_goto']=='restart') 
+if($_REQUEST['event']== "NewCall") 
 {
 
-    $collectInput = New CollectDtmf();
+    $collectInput = new CollectDtmf();
 	$collectInput->addPlayText('please enter the conference number followed by hash',3);
 	$collectInput->setMaxDigits('4'); //max inputs to be allowed
 	$collectInput->setTimeOut('4000');  //maxtimeout if caller not give any inputs
@@ -15,12 +16,20 @@ if($_REQUEST['event']== "NewCall" || $_SESSION['next_goto']=='restart')
     $_SESSION['next_goto']='conference';
 
 }
-else if($_SESSION['next_goto']=='conference')
+else if( $_REQUEST['event'] == "GotDTMF" && $_SESSION['next_goto']=='conference')
 {
      if($_REQUEST['data']=="")
      {
-         $r->addPlayText("You have not given any input");
-         $_SESSION['next_goto']='restart';
+        
+        $collectInput = new CollectDtmf();
+        $collectInput->addPlayText("You have not given any input");
+	$collectInput->addPlayText('please enter the conference number again followed by hash',3);
+	$collectInput->setMaxDigits('4'); //max inputs to be allowed
+	$collectInput->setTimeOut('4000');  //maxtimeout if caller not give any inputs
+	$collectInput->setTermChar('#');  
+	$r->addCollectDtmf($collectInput);
+        $_SESSION['next_goto']='conference';
+         
       }
      else if($_REQUEST['data']==$confno[0])
      {
