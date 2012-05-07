b2c3b300d6c83fb665a29ee88b124383fab1b2ef
anoop <anoop@ozonetel.com>
2012-05-07 12:28:05 +0530

    session updated

--- kookoophp/snippets/Playback dynamic text.php
+++ kookoophp/snippets/Playback dynamic text.php
@@ -1,5 +1,6 @@
 <?php
 //Description:Sample Code to playback a dynamic text
+session_start();
 require_once("response.php");//response.php is the kookoo xml preparation class file
 $r = new Response();
 $languages=array("Press 1 for English","Press 2 for Hindi","Press 3 for Kannada");
